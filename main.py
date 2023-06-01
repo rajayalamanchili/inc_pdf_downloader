8ed7b7ae53ea459ed867c847ed1ffc872b643d12
Raja Yalamanchili <raja.yalamanchili2@gmail.com>
2023-06-01 11:02:27 -0400

    feat: updated playwirght_test to async and capture api call; main.py async incomplete

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import json
 import re
 import os
+import asyncio
 from time import sleep
 import pandas as pd
-from playwright.sync_api import sync_playwright, expect
+from playwright.async_api import async_playwright
 
 
 ## get config data
 config_filename = "config.json"
 
-def get_config(filename):
 
-    try:
-       with open(filename) as f:
-           data = json.load(f)
-           return data
-    except Exception as e:
-        print("Error: ", e)
+def get_config(filename):
+    with open(filename) as f:
+        data = json.load(f)
+        return data
+
 
 config_data = get_config(config_filename)
 
@@ -29,51 +28,78 @@ outputDir = os.path.join(os.getcwd(), "downloads")
 if not os.path.exists(outputDir):
     os.mkdir(outputDir)
 
-##########################
-def getAuthPage(page, url):
 
+##########################
+async def handle_request(route):
+    response = await route.fulfill()
+    json_data = await response.json()
+
+    print(json_data)
+
+    return json_data
+
+
+async def getAuthPage(page, url):
     # open page click login
-    page.goto(url)
-    with page.expect_popup() as page1_info:
-        page.get_by_role("banner").get_by_role("link", name="Log in").click()
-    page1 = page1_info.value
+    await page.goto(url)
+    async with page.expect_popup() as page1_info:
+        await page.get_by_role("banner").get_by_role("link", name="Log in").click()
+    page1 = await page1_info.value
 
     print("Page login .... started")
 
     # enter login info and click submit
-    page1.get_by_placeholder("Email").click()
-    page1.get_by_placeholder("Email").fill(usrname)
-    page1.get_by_placeholder("Email").press("Tab")
-    page1.get_by_placeholder("Password").fill(passwd)
-    page1.get_by_role("button", name="Log in").click()
+    await page1.get_by_placeholder("Email").click()
+    await page1.get_by_placeholder("Email").fill(usrname)
+    await page1.get_by_placeholder("Email").press("Tab")
+    await page1.get_by_placeholder("Password").fill(passwd)
 
-    # TODO: add functionality to wait for page to completely load
-    page1.get_by_role("heading").filter(has_text=re.compile("Policies", re.IGNORECASE)).wait_for()
-    
+    await page1.route("*/api/policy", handle_request)
+
+    await page1.get_by_role("button", name="Log in").click()
+
+    # add functionality to wait for page to completely load
+    await page1.get_by_role("heading").filter(
+        has_text=re.compile("Policies", re.IGNORECASE)
+    ).wait_for()
+
     print("Page login .... done")
 
     return page1
 
+
 ##########################
 def getPolicyDetails(auth_home_page):
-
     print("Extracting homepage data .... started")
 
     # extract policy numbers
     policy_numbers = auth_home_page.locator(".policy-number-title").all_inner_texts()
-    
+
     # extract policy nicknames
     policy_nicknames = auth_home_page.locator(".policy-nickname").all_inner_texts()
-    
+
     # extract premiums and periods
-    policy_premiums_periods = auth_home_page.locator(".policy-info-value").all_inner_texts()
-    
+    policy_premiums_periods = auth_home_page.locator(
+        ".policy-info-value"
+    ).all_inner_texts()
+
     # combine data
-    policy_details = {"policy_numbers": policy_numbers, \
-                      "policy_nicknames": [policy_nicknames[i].replace("\n","").replace(",","|") for i in range(len(policy_nicknames))], \
-                      "policy_premiums": [policy_premiums_periods[i] for i in range(0,len(policy_premiums_periods),2)], \
-                      "policy_periods": [policy_premiums_periods[i].replace("\xa0-\xa0","-") for i in range(1,len(policy_premiums_periods),2)]}
-    
+    policy_details = {
+        "policy_numbers": policy_numbers,
+        "policy_nicknames": [
+            policy_nicknames[i].replace("\n", "").replace(",", "|")
+            for i in range(len(policy_nicknames))
+        ],
+        "policy_premiums": [
+            policy_premiums_periods[i]
+            for i in range(0, len(policy_premiums_periods), 2)
+        ],
+        "policy_periods": [
+            policy_premiums_periods[i].replace("\xa0-\xa0", "-")
+            for i in range(1, len(policy_premiums_periods), 2)
+        ],
+    }
+
     print("Extracting homepage data .... done")
 
     return policy_details
@@ -81,14 +107,14 @@ def getPolicyDetails(auth_home_page):
 
 ##########################
 
+
 def downloadPdf(context, auth_home_page, linkName, pdfName):
-    
     ### land on polcy document page
     auth_home_page.get_by_role("link", name=linkName).click()
-    
+
     # with context.expect_page() as myDocumentsInfo:
     #     auth_home_page.get_by_role("link", name=linkName).click()
-    
+
     # myDocumentsPage = myDocumentsInfo.value
 
     ### wait till page loads
@@ -96,12 +122,15 @@ def downloadPdf(context, auth_home_page, linkName, pdfName):
 
     ### click on document link
     with auth_home_page.expect_popup() as pdfPageInfo:
-        
-        rowNames = auth_home_page.get_by_role("row").filter(has_text=re.compile("Effective", re.IGNORECASE)).all()
-        
+        rowNames = (
+            auth_home_page.get_by_role("row")
+            .filter(has_text=re.compile("Effective", re.IGNORECASE))
+            .all()
+        )
+
         # assuming only one link per row
         rowNames[0].get_by_role("link").click()
-    
+
     pdfPage = pdfPageInfo.value
     pdfPage.get_by_role("button", name="Download").wait_for()
 
@@ -111,7 +140,7 @@ def downloadPdf(context, auth_home_page, linkName, pdfName):
     ### download pdf from page
     with pdfPage.expect_download() as downloadInfo:
         pdfPage.get_by_role("button", name="Download").click()
-    
+
     download = downloadInfo.value
     path = download.path()
 
@@ -122,13 +151,16 @@ def downloadPdf(context, auth_home_page, linkName, pdfName):
 
     ## back to homepage
     auth_home_page.get_by_role("menuitem", name="Homepage").click()
-    auth_home_page.get_by_role("heading").filter(has_text=re.compile("Policies", re.IGNORECASE)).wait_for()
-    
+    auth_home_page.get_by_role("heading").filter(
+        has_text=re.compile("Policies", re.IGNORECASE)
+    ).wait_for()
 
 
 def downloadPolicyDetailsPDF(context, auth_home_page, policyDetailsDict):
     print("Downloading pdfs .... started")
-    documentPageLinks = auth_home_page.get_by_label(re.compile("Get documents", re.IGNORECASE)).all()
+    documentPageLinks = auth_home_page.get_by_label(
+        re.compile("Get documents", re.IGNORECASE)
+    ).all()
     documentPageLinks = [link.get_attribute("aria-label") for link in documentPageLinks]
 
     totalLinks = len(documentPageLinks)
@@ -137,39 +169,44 @@ def downloadPolicyDetailsPDF(context, auth_home_page, policyDetailsDict):
         pdfName = (policyDetailsDict["policy_numbers"][i] + ".pdf").replace(" ", "_")
         pdfName = os.path.join(outputDir, pdfName)
 
-        print("\tDownloading pdf {:d}/{:d}".format( i+1, totalLinks))
+        print("\tDownloading pdf {:d}/{:d}".format(i + 1, totalLinks))
         downloadPdf(context, auth_home_page, linkName, pdfName)
-    
+
     print("Downloading pdfs .... done")
-   
 
 
 ##########################
-def getAllPolicyDetailsDownloadPDF(context, page, url):
-
+async def getAllPolicyDetailsDownloadPDF(context, page, url):
     # get autheticate page
-    auth_home_page = getAuthPage(page, url)
+    auth_home_page = await getAuthPage(page, url)
+
+    data = await auth_home_page.request.put(
+        "https://customer.safeco.com/accountmanager/api/policy"
+    )
 
     # extract policy details from homepage
-    policyDetailsDict = getPolicyDetails(auth_home_page)
+    policyDetailsDict = await getPolicyDetails(auth_home_page)
 
     # print(policyDetails)
     outputCSVFname = os.path.join(outputDir, "policy_details.csv")
     pd.DataFrame(policyDetailsDict).to_csv(outputCSVFname, index=False)
 
-
     # download policy details docs
-    policyDetailsDict = downloadPolicyDetailsPDF(context, auth_home_page, policyDetailsDict)
+    await downloadPolicyDetailsPDF(context, auth_home_page, policyDetailsDict)
+
 
 ##########################
-with sync_playwright() as p:
-    # browser = p.chromium.launch(downloads_path=outputDir)
-    browser = p.chromium.launch()
-    context = browser.new_context(accept_downloads=True)
-    page = context.new_page()
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        context = await browser.new_context(accept_downloads=True)
+        page = await context.new_page()
 
-    getAllPolicyDetailsDownloadPDF(context, page, url)
+        await getAllPolicyDetailsDownloadPDF(context, page, url)
 
-    # ---------------------
-    context.close()
-    browser.close()
+        # ---------------------
+        await context.close()
+        await browser.close()
+
+
+asyncio.run(main())
--- playwright_test.py
+++ playwright_test.py
@@ -4,27 +4,76 @@
 # sudo playwright install
 # sudo playwright install-deps
 # sudo pip install pandas
+import re
+import asyncio
+from playwright.async_api import async_playwright
 
-from playwright.sync_api import sync_playwright
 
-def get_prod_vals_from_page(page):
-    page.goto("https://bstackdemo.com/")
-    
-    print(page.title())
+async def handle_response(response):
+    if "api/products" in response.url:
+        print(await response.json())
 
-    total_products = page.locator(".shelf-container").locator(".shelf-item").count()
+
+async def get_prod_vals_from_page(page):
+    await page.goto("https://bstackdemo.com/", wait_until="networkidle")
+
+    await page.get_by_role("link", name="Sign In").click()
+    await page.locator("div").filter(has_text=re.compile(r"^Select Username$")).nth(
+        2
+    ).click()
+    await page.get_by_text("demouser", exact=True).click()
+    await page.locator("div").filter(has_text=re.compile(r"^Select Password$")).nth(
+        2
+    ).click()
+    await page.get_by_text("testingisfun99", exact=True).click()
+
+    # # page.on("response", handle_response)
+
+    async with page.expect_response(
+        lambda request: "/api/products" in request.url
+    ) as response_info:
+        await page.get_by_role("button", name="Log In").click()
+        await page.get_by_text("Logout").wait_for()
+
+    response = await response_info.value
+
+    print(await response.json())
+
+    # # await page.get_by_role("button", name="Log In").click()
+
+    print(await page.title())
+
+    total_products = (
+        await page.locator(".shelf-container").locator(".shelf-item").count()
+    )
     print("Total products: ", total_products)
     for prod in range(total_products):
-        prod_title = page.locator(".shelf-container").locator(".shelf-item").locator(".shelf-item__title").nth(prod).all_inner_texts()[0]
-        prod_price = page.locator(".shelf-container").locator(".shelf-item").locator(".shelf-item__price").nth(prod).locator(".val").all_inner_texts()[0]
+        prod_title = (
+            await page.locator(".shelf-container")
+            .locator(".shelf-item")
+            .locator(".shelf-item__title")
+            .nth(prod)
+            .all_inner_texts()
+        )
+        prod_price = (
+            await page.locator(".shelf-container")
+            .locator(".shelf-item")
+            .locator(".shelf-item__price")
+            .nth(prod)
+            .locator(".val")
+            .all_inner_texts()
+        )
 
-        print(str(prod+1) + "\t" + str(prod_title) + "\t" + str(prod_price))
+        print(str(prod + 1) + "\t" + str(prod_title[0]) + "\t" + str(prod_price[0]))
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    get_prod_vals_from_page(page)
 
-    browser.close()
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        await get_prod_vals_from_page(page)
 
-    
+        await browser.close()
+
+
+asyncio.run(main())
